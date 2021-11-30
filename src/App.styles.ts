a07b5059b2ebc0f309874de2d446305ab866a0a3
AleexCaires <tiagoaleex98@gmail.com>
2021-11-30 19:34:27 +0000

    Button Styling

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -41,23 +41,46 @@ export const Wrapper = styled.div `
         --moz-background-clip: text;
         --webkit-text-fill-color: color;
         filter: drop-shadow(2px 2px #008583);
-        font-size: 50px;
+        font-size: 60px;
         text-align:center;
         margin: 20px;
         font-weight: 400;
     }
 
     .start, .next{
+        width: 200px;
+        font-size: 16px;
+        font-weight: 600;
+        color: black;
         cursor: pointer;
-        background: linear-gradient(180deg, #fff, #ffcc91)
-        border: 2px solid #d38558;
-        box-shadow: 0px 5px 10px rgba(0,0,0,0.25);
-        border-radius: 10px;
-        height:40px;
-        margin:20px 0;
-        padding: 0 40px;
+        background-color: white;
+        margin: 20px;
+        height: 55px;
+        text-align:center;
+        border: none;
+        background-size: 300% 100%;
+        border-radius: 50px;
+        --moz-transition: all .6s ease-in-out;
+        --o-transition: all .6s ease-in-out;
+        --webkit-transition: all .6s ease-in-out;
+        transition: all .6s ease-in-out;
     }
     .start {
         max-width: 200px;
+    }
+    .next{
+        background-color: #ebfeff;
+        border-radius: 1px solid block;
+    }
+    .next:hover{
+        font-weight: 800;
+    }
+    .start:hover {
+        background-position: 100% 0;
+        --moz-transition: all .6s ease-in-out;
+        --o-transition: all .6s ease-in-out;
+        --webkit-transition: all .6s ease-in-out;
+        transition: all .6s ease-in-out;
+        background-image: linear-gradient(315deg, #abe9cd 0%, #3eadcf 74%);
     }
 `
--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export type AnswerObject= {
   correctAnswer: string;
 }
 
-const TOTAL_QUESTIONS = 10;
+const TOTAL_QUESTIONS = 15;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -81,7 +81,7 @@ const App = () => {
     <Wrapper>
       <h1>React Quiz</h1>
         {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-          <button className='start' onClick={startTrivia}>
+          <button className='start draw-border' onClick={startTrivia}>
       Start
           </button>
         ) : null}
